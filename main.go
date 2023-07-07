ea3d1c9a71795a5827e130baa1edcaec391cd8dd
kia-007 <rizkiafh180@gmail.com>
2023-07-07 10:06:13 +0700

    crud with mvc pattern

---
+++ main.go
@@ -0,0 +1,25 @@
+// main.go
+package main
+
+import (
+	"api/Config"
+	"api/Models"
+	"api/Routes"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
